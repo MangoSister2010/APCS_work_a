f2c3640bc03866d91e5aee77d2dab82628b08e46
MangoSister2010 <clairelee28@marlborough.org>
2024-09-18 13:24:27 -0700

    assignment

--- unit1/Geometric.java
+++ unit1/Geometric.java
@@ -1,8 +1,11 @@
 public class Geometric {
     public static void main(String[] args) {
-        for (int i = 3; i < 10; i*2) {
-            System.out.println(term); // Print the current term
-            i *= 2;      // Update the term by multiplying with the common ratio
+        int term = 1;
+        int ratio = 2;
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(term);
+            term *= ratio;
         }
     }
 }
