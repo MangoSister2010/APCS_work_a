0c914f2703385d7ff56fa074be886265fc2cd95e
MangoSister2010 <clairelee28@marlborough.org>
2024-09-18 13:17:41 -0700

    assignment

--- unit1/Fibonacci.java
+++ unit1/Fibonacci.java
@@ -1,15 +1,18 @@
-public class Fibonacci {
-    public static void main(String[] args) {
+public class Fibonacci 
+{
+    public static void main(String[] args) 
+    {
 
         int a = 1; 
         int b = 1; 
 
         System.out.println(a);
        
-        }
+        
 
         
-        for (int i = 2; i < 30; i++) {
+        for (int i = 2; i < 30; i++) 
+        {
             int c = a + b; 
             System.out.println(c); 
             
@@ -17,4 +20,5 @@ public class Fibonacci {
             b = c; 
         }
     }
+}
 
