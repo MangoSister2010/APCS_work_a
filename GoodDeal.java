6e2b33fb76d4cf16eb7288df3cfba5bee66fa38c
MangoSister2010 <clairelee28@marlborough.org>
2024-09-23 20:25:59 -0700

    Add files via upload

---
+++ GoodDeal.java
@@ -0,0 +1,23 @@
+public class GoodDeal 
+{
+    public static boolean GoodDeal(double originalPrice, double salePrice) 
+    {
+        if(salePrice < 0.75 * originalPrice)
+        {
+            return true;
+        }
+      
+        else
+        {
+            return false;   
+        }
+       
+    }
+
+    public static void main(String[] args) 
+    {
+       System.out.println(GoodDeal(100,70));
+       System.out.println(GoodDeal(100,80));
+
+    }
+}
