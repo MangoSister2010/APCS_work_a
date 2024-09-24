157e48f0673134a849691079f30b7844ea01e28b
MangoSister2010 <clairelee28@marlborough.org>
2024-09-24 09:23:41 -0700

    homeowork

---
+++ unit2/Diff21.java
@@ -0,0 +1,21 @@
+public class Diff21 
+{
+    public static int diff21(int n) 
+    {
+        if (n > 21) 
+        {
+            return 2 * (n - 21); 
+        } 
+        
+        else 
+        {
+            return 21 - n; 
+        }
+    }
+
+    public static void main(String[] args) //test
+    {
+        System.out.println(diff21(19));
+        System.out.println(diff21(30));
+    }
+}
---
+++ unit2/Vampire.java
@@ -0,0 +1,21 @@
+public class Vampire 
+{
+    public static boolean Vampire(float hour, boolean awake) 
+    {
+        if (awake && (hour < 6.0 || hour >= 22.0)) //awake = true
+        {
+            return true; 
+        } 
+        else if (!awake && (hour >= 6.0 && hour < 22.0)) //not awake = false
+        {
+            return true; 
+        }
+        return false; // Not a vampire 
+    }
+
+    public static void main(String[] args) //test
+    {
+        System.out.println(Vampire(5.0f, true)); // f is used for float or double
+        System.out.println(Vampire(10.0f, false));
+    }
+}
